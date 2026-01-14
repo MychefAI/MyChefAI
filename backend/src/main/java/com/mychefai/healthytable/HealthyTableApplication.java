5f909c5f93c41d4d2d3e25f3d8230042c87c5dce
김준서\rlawn <a01028399047@gmail.com>
2026-01-14 14:18:06 +0900

    [FEAT]test입니다!

--- backend/src/main/java/com/mychefai/healthytable/HealthyTableApplication.java
+++ backend/src/main/java/com/mychefai/healthytable/HealthyTableApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HealthyTableApplication {
     public static void main(String[] args) {
         SpringApplication.run(HealthyTableApplication.class, args);
+        System.out.println("실행 성공!");
     }
 }
